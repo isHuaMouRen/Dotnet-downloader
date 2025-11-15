c1cb1bc89ddd621fdb43b062b78892652c97e655
HuaMouRen <2825338723@qq.com>
2025-11-15 14:10:17 +0800

    feat(globals): 添加全局类

---
+++ DotnetDownloader/Class/Globals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using HuaZi.Library.Logger;
+
+namespace DotnetDownloader.Class
+{
+    public static class Globals
+    {
+        //变量
+        public static class Var
+        {
+            public static readonly string ExecutePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";//执行位置
+            public static readonly string Version = $"1.0.0-alpha.1";
+        }
+
+        //对象
+        public static class Obj
+        {
+            public static Logger logger = new Logger(Path.Combine(Var.ExecutePath, "Logs"));
+        }
+    }
+}
