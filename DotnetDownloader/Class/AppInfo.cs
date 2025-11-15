257042a95dc793b864069959710ff2a9275a394a
HuaMouRen <2825338723@qq.com>
2025-11-15 14:33:14 +0800

    ref(globals): 拆分全局类 Globals

---
+++ DotnetDownloader/Class/AppInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetDownloader.Class
+{
+    public static class AppInfo
+    {
+        public static readonly string ExecutePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";//执行位置
+        public static readonly string Version = $"1.0.0-alpha.1";//版本
+    }
+}
---
+++ DotnetDownloader/Class/AppLogger.cs
@@ -0,0 +1,16 @@
+﻿using HuaZi.Library.Logger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DotnetDownloader.Class.Globals;
+
+namespace DotnetDownloader.Class
+{
+    public class AppLogger
+    {
+        public static Logger logger = new Logger(Path.Combine(AppInfo.ExecutePath, "Logs"));
+    }
+}
