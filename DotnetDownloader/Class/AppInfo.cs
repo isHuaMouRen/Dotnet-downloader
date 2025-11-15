ac9e3908672f343c2eebbb42103501be286af0ab
HuaMouRen <2825338723@qq.com>
2025-11-15 15:08:32 +0800

    feat(app-info): 添加.NET索引文件url

--- DotnetDownloader/Class/AppInfo.cs
+++ DotnetDownloader/Class/AppInfo.cs
@@ -12,5 +12,6 @@ namespace DotnetDownloader.Class
     {
         public static readonly string ExecutePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";//执行位置
         public static readonly string Version = $"1.0.0-alpha.1";//版本
+        public static readonly string DotnetIndexUrl = $"https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json";
     }
 }
